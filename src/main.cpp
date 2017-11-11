35650e6b19522857932048e6231c1fc7e3115cd4
benjithompson <mail@benjithompson.com>
2017-11-10 23:30:04 -0600

    using aREST_UI

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,13 @@
 // Import required libraries
 #include <WiFi.h>
 #include <aREST.h>
+#include <aREST_UI.h>
+
+
 
 // Create aREST instance
-aREST rest = aREST();
+aREST_UI rest = aREST_UI();
+
 
 // WiFi parameters
 char *ssid = "pluviophile2.4";
@@ -20,16 +24,12 @@ char *password = "Youshallnotpassword!";
 // Create an instance of the server
 WiFiServer server(80);
 
-const int led1 = 16; // the number of the LED pin
-const int led2 = 17; // the number of the LED pin
-
 // Client variables
 char linebuf[80];
 int charcount = 0;
 
 // Variables to be exposed to the API
-int temperature;
-int humidity;
+int led;
 
 // Declare functions to be exposed to the API
 int ledControl(String command);
@@ -43,12 +43,24 @@ void setup()
 
   // set pin as output
   pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(32, OUTPUT);
+  // pinMode(35, OUTPUT);
+  // pinMode(32, OUTPUT);
+
+  rest.title("aREST UI Demo");
+  
+  rest.slider(34);
+
+  rest.button(2);
 
   // Init variables and expose them to REST API
-  temperature = 24;
-  humidity = 40;
-  rest.variable("temperature", &temperature);
-  rest.variable("humidity", &humidity);
+  led = 0;
+  rest.variable("Led", &led);
+
+  // Labels
+  rest.label("temperature");
+  rest.label("humidity");
+
 
   // Function to be exposed
   rest.function("led", ledControl);
@@ -84,100 +96,19 @@ void setup()
 void loop()
 {
 
-  // // Handle REST calls
   WiFiClient client = server.available();
-  // if (!client) {
-  //   return;
-  // }
-  // while(!client.available()){
-  //   delay(1);
-  // }
-  // rest.handle(client);
-
-  if (client)
-  {
-    Serial.println("New client");
-    memset(linebuf, 0, sizeof(linebuf));
-    charcount = 0;
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected())
-    {
-      if (client.available())
-      {
-        char c = client.read();
-        Serial.write(c);
-        //read char by char HTTP request
-        linebuf[charcount] = c;
-        if (charcount < sizeof(linebuf) - 1)
-          charcount++;
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank)
-        {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close"); // the connection will be closed after completion of the response
-          client.println();
-          client.println("<!DOCTYPE HTML><html><head>");
-          client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
-          client.println("<h1>ESP32 - Web Server</h1>");
-          client.println("<p>LED #1 <a href=\"on1\"><button>ON</button></a>&nbsp;<a href=\"off1\"><button>OFF</button></a></p>");
-          client.println("<p>LED #2 <a href=\"on2\"><button>ON</button></a>&nbsp;<a href=\"off2\"><button>OFF</button></a></p>");
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n')
-        {
-          // you're starting a new line
-          currentLineIsBlank = true;
-          if (strstr(linebuf, "GET /on1") > 0)
-          {
-            Serial.println("LED 1 ON");
-            digitalWrite(led1, HIGH);
-          }
-          else if (strstr(linebuf, "GET /off1") > 0)
-          {
-            Serial.println("LED 1 OFF");
-            digitalWrite(led1, LOW);
-          }
-          else if (strstr(linebuf, "GET /on2") > 0)
-          {
-            Serial.println("LED 2 ON");
-            digitalWrite(led2, HIGH);
-          }
-          else if (strstr(linebuf, "GET /off2") > 0)
-          {
-            Serial.println("LED 2 OFF");
-            digitalWrite(led2, LOW);
-          }
-          // you're starting a new line
-          currentLineIsBlank = true;
-          memset(linebuf, 0, sizeof(linebuf));
-          charcount = 0;
-        }
-        else if (c != '\r')
-        {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
+  if(!client){
+    return;
   }
+  while(!client.available()){
+    delay(1);
+  }
+  rest.handle(client);
 }
 
 // Custom function accessible by the API
 int ledControl(String command)
 {
-
   // Get state from command
   int state = command.toInt();
   digitalWrite(6, state);
@@ -195,3 +126,5 @@ void wifiConnectedStatusLED()
     delay(75);
   }
 }
+
+
