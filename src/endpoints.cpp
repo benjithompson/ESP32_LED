57c6d89e0b3d1f26e768aa881462b4074513e1d2
benjithompson <mail@benjithompson.com>
2017-11-14 22:03:58 -0600

    separated arest endpoint functions

---
+++ src/endpoints.cpp
@@ -0,0 +1,68 @@
+
+#include <Arduino.h>
+#include "endpoints.h"
+
+int testFunction(String command)
+{
+}
+// Custom function accessible by the API
+int ledControl(String command)
+{
+    // Get state from command
+    int state = command.toInt();
+    Serial.println(state);
+
+    switch (state)
+    {
+    case 1:
+        Serial.println("Case1");
+        xTaskCreate(&taskBlinkOne, "blinkOne", 512, NULL, 10, NULL);
+        break;
+    case 2:
+        Serial.println("Case2");
+        xTaskCreate(&taskBlinkTwo, "blinkTwo", 512, NULL, 10, NULL);
+        break;
+    default:
+        Serial.println("default");
+        break;
+    }
+    return state;
+}
+
+void taskBlinkOne(void *p)
+{
+    blinkOne();
+    vTaskDelete(NULL);
+}
+
+void blinkOne()
+{
+
+    for (int i = 0; i < 10; i++)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(100);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+}
+
+void taskBlinkTwo(void *p)
+{
+    blinkTwo();
+    vTaskDelete(NULL);
+}
+
+void blinkTwo()
+{
+
+    for (int i = 0; i < 10; i++)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(100);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+}
---
+++ src/endpoints.h
@@ -0,0 +1,6 @@
+void taskBlinkOne(void* p);
+void blinkOne();
+void taskBlinkTwo(void* p);
+void blinkTwo();
+int ledControl(String command);
+int testFunction(String command);
